f3e72fc27d88a591406ba86dc8705f6abda422db
adrian-gvr <adrian82gavril@gmail.com>
2020-02-25 23:59:44 +0100

    commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,13 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+function fibo(z, a, b) {
+    let y = b;
+    console.log(a);
+    b += a;
+    if (z >= a)
+        return a;
+    a = fibo(z, a, b);
+    return a;
+}
+console.log(fibo(30, 0, 1));
